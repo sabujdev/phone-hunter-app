833011471241e4e179471f4318fbfe0d90f88f0d
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-03-02 00:32:28 +0600

    all update

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
+
+
+// const toppleSpinner = displayStyle =>{
+//     document.getElementById('spinner').style.display =displayStyle;
+// }
+// const toppleSearch = displayStyle =>{
+//     document.getElementById('main').style.display =displayStyle;
+// }
+
 //  search button click to find phone 
 const searchButton = () =>{
     const searchInput = document.getElementById('search-input')
     const searchText = searchInput.value ;
     searchInput.value = '';
-    // console.log(searchText)
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(res =>res.json())
     .then(data => displayProduc(data.data))
 } 
+
 // product show to UI 
 const displayProduc = (products) =>{
     const error = document.getElementById('error');
     const main = document.getElementById('main');
-    // console.log(products)
     if(products.length === 0){
         console.log('name not found')
         error.innerText = "Please type your phone's name."
         main.style.display = 'none'
-       
     }else{
         main.style.display = 'block'
         error.innerText = '';
         const productContainer = document.getElementById('product_container')
-                const fist20Product = products.splice(0, 20);
-                 productContainer.textContent = ''; 
-                    fist20Product.forEach(product =>{
-                    const newDiv = document.createElement('div')
-                newDiv.innerHTML = `
-                <div class="card p-2 m-2 ">
+         const fist20Product = products.splice(0, 20);
+            productContainer.textContent = ''; 
+             fist20Product.forEach(product =>{
+         const newDiv = document.createElement('div')
+             newDiv.innerHTML = `
+                <div class="card p-2 m-2 text-center">
                     <img class="w-50 mx-auto" src="${product.image}" class="card-img-top" alt="...">
                     <div class="card-body">
                      <h5 class="card-title">${product.phone_name}</h5>
@@ -38,7 +45,7 @@ const displayProduc = (products) =>{
                  <button onclick="productDetailBtn('${product.slug}')" class="btn btn-info m-3 border text-cerner">
                  View details</button></a>
                  </div>`;
-            productContainer.appendChild(newDiv)       
+        productContainer.appendChild(newDiv)       
     })
 
 }
@@ -56,15 +63,15 @@ const ulr = `https://openapi.programming-hero.com/api/phone/${slugId}`
     .then(data =>showDetails(data.data))
 }
 
+// detail btn click to show product details 
 const showDetails = (singleProduct) =>{
-// console.log(singleProduct)
 const detailInfo = document.getElementById('detail_info');
         detailInfo.innerHTML =''
        const div = document.createElement('div');
         div.classList.add('card')
         div.innerHTML = `
         <img class="w-50 p-2 mx-auto" src="${singleProduct.image}" class="card-img-top" alt="meal">
-        <div>
+        <div class = "mx-auto text-center">
             <h3 class="card-title">${singleProduct.name}</h3>
             <h5 class="card-title">${singleProduct.brand}</h5>
             <p class="card-text">${singleProduct.releaseDate ? singleProduct.releaseDate: 'release date not found'}</p>
