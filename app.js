f50b22d0b3f18033fb9285376861fa406df4e0fa
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-03-01 10:09:40 +0600

    api added or fetch data

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-
+//  search button click to find phone 
 const searchButton = () =>{
     const searchInput = document.getElementById('search-input')
-    const searchInputText = searchInput.value ;
-    console.log(searchInputText)
+    const searchText = searchInput.value ;
+    console.log(searchText)
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+    .then(res =>res.json())
+    .then(data => console.log(data.data))
 } 
